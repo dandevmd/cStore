e7dcc5b99e9345c5b98c1a800b3e641f5c69a1d9
dandev <danmunteanu420@gmail.com>
2022-04-06 15:48:29 +0300

    close dropDown on outside click

--- src/components/card-dropdown/CardDropdown.jsx
+++ src/components/card-dropdown/CardDropdown.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import DropdownContext from '../../contexts/DropdownContext'
@@ -8,21 +8,41 @@ import CartItem from '../cart-item/CartItem'
 import './cardDropdown.styles.scss'
 
 const CardDropdown = () => {
-  const { cartItems } = useContext(DropdownContext)
+  const { cartItems, setIsOpen, isOpen } = useContext(DropdownContext)
   const navigate = useNavigate()
+  const ref = useRef()
 
   const handleCheckout = () => {
     navigate('/checkout')
   }
 
+  useEffect(() => {
+    const checkIfClickedOutside = e => {
+      
+      if (isOpen && ref.current && !ref.current.contains(e.target)) {
+        setIsOpen(false)
+      }
+    }
+
+    document.addEventListener("mousedown", checkIfClickedOutside)
+
+    return () => {
+      document.removeEventListener("mousedown", checkIfClickedOutside)
+    }
+  }, [isOpen])
+
+  
   return (
-    <div className='cart-dropdown-container' >
+    <div className='cart-dropdown-container' 
+    ref={ref}
+      //  onMouseLeave={() => setIsOpen(false)}
+    >
       <div className='cart-items' >
         {cartItems.map(item => (
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button onClick={handleCheckout}>CHECKOUT</Button>
+      <Button onClick={handleCheckout} >CHECKOUT</Button>
     </div>
   )
 }
